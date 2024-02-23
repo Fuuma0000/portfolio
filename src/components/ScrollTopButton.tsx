398ddfd1f1a035641e0efc0105782235390f9333
Fuuma <masa81hiro69@gmail.com>
2024-02-23 18:32:06 +0900

    feat: ボタンが上に上がるようにできた

--- src/components/ScrollTopButton.tsx
+++ src/components/ScrollTopButton.tsx
@@ -1,20 +1,30 @@
 import { animateScroll as scroll } from 'react-scroll';
+import { motion, useAnimation } from 'framer-motion';
 
 const ScrollToTopButton = () => {
+  const controls = useAnimation();
+
   const scrollToTop = () => {
     scroll.scrollToTop({
-      duration: 500, // アニメーションの時間（ミリ秒）
-      smooth: 'easeInOutQuad', // イージングの指定
+      duration: 500,
+      smooth: 'easeInOutQuad',
     });
+
+    // トップにスクロールするときにアニメーションを開始
+    controls.start({ y: -1980 });
   };
 
   return (
-    <button
-      className="fixed bottom-4 right-4 rounded-md bg-blue-500 px-4 py-2 text-white"
-      onClick={scrollToTop}
-    >
-      Scroll To Top
-    </button>
+    <div className="bg-red-500">
+      <motion.button
+        initial={{ opacity: 100, y: 0 }}
+        animate={controls}
+        className={`fixed bottom-4 right-4 rounded-md bg-red-500 px-4 py-2 text-white`}
+        onClick={scrollToTop}
+      >
+        トップにスクロール
+      </motion.button>
+    </div>
   );
 };
 
