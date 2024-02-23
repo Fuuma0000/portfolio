350b84d378976ada816e7110e083f726fbf8d8b3
Fuuma <masa81hiro69@gmail.com>
2024-02-23 16:52:54 +0900

    chore: 絶対パスインポートに変更

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,9 @@
 import Header from '@/components/Header';
 import Projects from '@/components/Projects';
 import About from '@/components/About';
-import Links from './components/Links';
-import Profile from './components/Profile';
-import Footer from './components/Footer';
+import Links from '@/components/Links';
+import Profile from '@/components/Profile';
+import Footer from '@/components/Footer';
 
 const App = () => {
   const components = [Header, Profile, About, Links, Projects, Footer];
