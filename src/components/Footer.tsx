ea56ffeeab3f1393b8147f8fa33acdbeb0029ba1
Fuuma <masa81hiro69@gmail.com>
2024-02-23 14:44:11 +0900

    feat: フッターを追加

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,17 @@
-const Footer = () => {
-  return <div>Footer</div>;
+// const Footer = () => {
+//   return <div>Footer</div>;
+// };
+
+// export default Footer;
+
+const Footer = ({ index }: { index: number }) => {
+  return (
+    <div className={`bg-${index % 2 === 0 ? 'even' : 'odd'} `} id="footer">
+      <footer className="mx-auto max-w-7xl">
+        <p className="text-center text-white">© 2024 - Copyright Fuuma.</p>
+      </footer>
+    </div>
+  );
 };
 
 export default Footer;
