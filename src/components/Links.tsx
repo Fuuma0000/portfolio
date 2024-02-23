b34bed996c124b6a20c278f70a70153cf8b276b5
Fuuma <masa81hiro69@gmail.com>
2024-02-23 16:28:20 +0900

    feat: レスポンシブの対応をgridでするように

--- src/components/Links.tsx
+++ src/components/Links.tsx
@@ -42,34 +42,30 @@ const Links = ({ index }: { index: number }) => {
 
   return (
     <div className={`bg-${index % 2 === 0 ? 'even' : 'odd'}`} id="links">
-      <div className="mx-auto flex h-full max-w-7xl flex-col justify-center md:h-screen">
+      <div className="mx-auto flex h-screen max-w-7xl flex-col justify-center">
         {/* 真ん中に大きく表示する */}
         <h1 className="mb-8 text-center text-3xl font-semibold text-primary md:mb-0">
           Links
         </h1>
         {/* レスポンシブ用のdiv */}
-        <div className="mt-8">
-          {/* リンクを表示 */}
-          <ul className="flex flex-row items-center justify-center gap-10">
-            {linkData.map((link, index) => (
-              <li key={index}>
-                <a
-                  href={link.href}
-                  target="_blank"
-                  className="flex cursor-pointer flex-col items-center"
-                >
-                  <img
-                    src={link.src}
-                    alt=""
-                    className="h-[120px] w-[120px] rounded-full"
-                  />
-                  <span className="text-sm text-white opacity-60">
-                    {link.text}
-                  </span>
-                </a>
-              </li>
-            ))}
-          </ul>
+        <div className="mt-8 grid grid-cols-2 gap-4 md:flex md:flex-wrap md:justify-center">
+          {linkData.map((link, index) => (
+            <a
+              key={index}
+              href={link.href}
+              target="_blank"
+              className="mx-2 mb-4 flex flex-col items-center"
+            >
+              <img
+                src={link.src}
+                alt=""
+                className="h-[60px] w-[60px] rounded-full md:h-[80px] md:w-[80px]"
+              />
+              <span className="mt-2 text-xs text-white opacity-60 md:text-sm">
+                {link.text}
+              </span>
+            </a>
+          ))}
         </div>
       </div>
     </div>
