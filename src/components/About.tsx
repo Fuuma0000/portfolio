010de506faf8e91a82addc704a534c68a5408307
Fuuma <masa81hiro69@gmail.com>
2024-02-23 12:55:18 +0900

    feat: 自己紹介完成

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,5 +1,25 @@
-const About = () => {
-  return <div>About</div>;
+const About = ({ index }: { index: number }) => {
+  return (
+    <div className={`bg-${index % 2 === 0 ? 'even' : 'odd'} `} id="about">
+      <div className="mx-auto flex h-full max-w-7xl  flex-col justify-center md:h-screen ">
+        {/* 真ん中に大きく表示する */}
+        <h1 className="mb-8 text-center text-3xl font-semibold text-primary md:mb-0">
+          About
+        </h1>
+        {/* レスポンシブ用のdiv */}
+        <div className="mt-8 flex items-center justify-center text-white">
+          {/* 自己紹介 */}
+          <p className="min-w-[100px] max-w-[600px] text-base">
+            2022年にECCコンピュータ専門学校に入学し、プログラミングを学び始めました。
+            <br />
+            インフラエンジニアを志望しているのですが、フロント・バック・モバイルなども触っています。
+            <br />
+            サーバー側が好きなのですが、最近はAWSも面白いと思い、勉強しています。
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default About;
