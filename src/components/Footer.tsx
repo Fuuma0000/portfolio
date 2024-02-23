b2765efc4899b908dc731d9d65c362642a2cf959
Fuuma <masa81hiro69@gmail.com>
2024-02-23 15:24:55 +0900

    feat: py付けて少し見やすく

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,7 +7,7 @@
 const Footer = ({ index }: { index: number }) => {
   return (
     <div className={`bg-${index % 2 === 0 ? 'even' : 'odd'} `} id="footer">
-      <footer className="mx-auto max-w-7xl">
+      <footer className="mx-auto max-w-7xl py-1">
         <p className="text-center text-white">© 2024 - Copyright Fuuma.</p>
       </footer>
     </div>
