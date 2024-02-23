b25755e11d8cbab1c8b46a57184b68fa66c0c359
Fuuma <masa81hiro69@gmail.com>
2024-02-23 18:53:23 +0900

    feat: 上に行って、下から表示出来るようにした

--- src/components/ScrollTopButton.tsx
+++ src/components/ScrollTopButton.tsx
@@ -4,14 +4,29 @@ import { motion, useAnimation } from 'framer-motion';
 const ScrollToTopButton = () => {
   const controls = useAnimation();
 
-  const scrollToTop = () => {
+  const scrollToTop = async () => {
+    // トップにスクロール
     scroll.scrollToTop({
       duration: 500,
       smooth: 'easeInOutQuad',
     });
 
-    // トップにスクロールするときにアニメーションを開始
+    // アニメーションが開始されたら非表示にする
     controls.start({ y: -1980 });
+
+    // 一定時間待機
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+    controls.start({ visibility: 'hidden' });
+
+    // アニメーションが完了したら元の位置に戻し、表示する
+    await controls.start({
+      y: 100,
+      opacity: 100,
+      visibility: 'hidden',
+      transition: { ease: 'easeInOut' },
+    });
+
+    controls.start({ y: 0, visibility: 'visible' });
   };
 
   return (
@@ -20,7 +35,9 @@ const ScrollToTopButton = () => {
         initial={{ opacity: 100, y: 0 }}
         animate={controls}
         className={`fixed bottom-4 right-4 rounded-md bg-red-500 px-4 py-2 text-white`}
-        onClick={scrollToTop}
+        onClick={() => {
+          scrollToTop();
+        }}
       >
         トップにスクロール
       </motion.button>
