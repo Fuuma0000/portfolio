bdf0792b0e6b03955628a4a1d1f1bc14f8fecdf0
Fuuma <masa81hiro69@gmail.com>
2024-02-24 17:08:56 +0900

    feat: 開いた時にトーストを表示するように

--- src/App.tsx
+++ src/App.tsx
@@ -7,9 +7,14 @@ import Profile from '@/components/Profile';
 import Footer from '@/components/Footer';
 import ScrollToTopButton from '@/components/ScrollTopButton';
 import Test from '@/components/Test';
+import { toast } from 'react-toastify';
+import { useEffect } from 'react';
 
 const App = () => {
   const components = [Header, Profile, About, Links, Projects, Footer, Test];
+  useEffect(() => {
+    toast.success('Fuumaのサイトにようこそ', {});
+  }, []);
 
   return (
     <div className="mx-auto">
