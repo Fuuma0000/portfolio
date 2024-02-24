a3a12b97dcb57135551c71ff67edf96a28c3b575
Fuuma <masa81hiro69@gmail.com>
2024-02-24 20:26:23 +0900

    feat: とりあえず目が表示され動くように

--- src/components/NotFound.tsx
+++ src/components/NotFound.tsx
@@ -1,8 +1,59 @@
-const NotFound = () => {
+import { Canvas } from '@react-three/fiber';
+import { useState } from 'react';
+import { TextureLoader } from 'three';
+import eyeSvg from '@/assets/textures/eye.svg';
+
+interface EyeProps {
+  rotation: {
+    x: number;
+    y: number;
+  };
+}
+
+const Eye: React.FC<EyeProps> = ({ rotation }) => {
+  const textureLoader = new TextureLoader();
+  const eyeTexture = textureLoader.load(eyeSvg);
+
   return (
-    <div className="flex h-screen flex-col items-center justify-center">
-      <h1 className="text-4xl font-bold">404</h1>
-      <p>ページが見つかりません</p>
+    <mesh rotation={[rotation.x, rotation.y, 0]}>
+      <sphereGeometry args={[1, 32, 32]} />
+      <meshBasicMaterial map={eyeTexture} />
+    </mesh>
+  );
+};
+
+const NotFound = () => {
+  const [rotation, setRotation] = useState({ x: 0, y: 0 });
+
+  const handleMouseMove = (event: React.PointerEvent<HTMLDivElement>) => {
+    const { clientX, clientY } = event;
+    const mouse = {
+      x: (clientX / window.innerWidth) * 2 - 1,
+      y: -(clientY / window.innerHeight) * 2 + 1,
+    };
+
+    // マウスの位置に合わせてボールを回転させる
+    const targetRotationX = -mouse.y * Math.PI * 0.25;
+    const targetRotationY = mouse.x * Math.PI * 0.25;
+
+    console.log(targetRotationX, targetRotationY);
+
+    setRotation({
+      x: targetRotationX,
+      y: targetRotationY,
+    });
+  };
+
+  return (
+    <div className="h-screen w-screen">
+      <Canvas
+        style={{ cursor: 'grab', position: 'absolute' }}
+        onPointerMove={handleMouseMove}
+        camera={{ position: [0, 0, 5], fov: 75 }}
+      >
+        <ambientLight intensity={0.5} />
+        <Eye rotation={rotation} />
+      </Canvas>
     </div>
   );
 };
