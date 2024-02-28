7f74cd9850110d1327b27404311c868b666d0676
Fuuma <masa81hiro69@gmail.com>
2024-02-28 20:30:43 +0900

    feat: 状態をrecoilで管理するように

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,8 @@
 import { Bars3BottomRightIcon, XMarkIcon } from '@heroicons/react/24/solid';
 import { Link } from 'react-scroll';
 import { useEffect, useState } from 'react';
+import { useRecoilState } from 'recoil';
+import { isDarkModeState } from '@/state/isDarkModeState';
 
 const Header = ({ index }: { index: number }) => {
   let Links = [
@@ -11,16 +13,25 @@ const Header = ({ index }: { index: number }) => {
   ];
 
   let [open, setOpen] = useState(false);
-  const [isDarkMode, setIsDarkMode] = useState(() => {
-    // LocalStorageから保存された状態を取得する
-    return localStorage.getItem('isDarkMode') === 'true';
-  });
+  const [isDarkMode, setIsDarkMode] = useRecoilState(isDarkModeState);
 
   const toggleDarkMode = () => {
-    setIsDarkMode(!isDarkMode);
-    // LocalStorageに状態を保存する
+    setIsDarkMode((prev) => !prev);
     localStorage.setItem('isDarkMode', String(!isDarkMode));
   };
+
+  // 初回レンダリング時にlocalStorageの値を読み込み、Recoilの状態を初期化する
+  useEffect(() => {
+    const storedValue = localStorage.getItem('isDarkMode');
+    console.log(storedValue);
+    if (storedValue !== null) {
+      setIsDarkMode(storedValue === 'true' ? true : false);
+    } else {
+      console.log('set');
+      console.log(isDarkMode);
+      localStorage.setItem('isDarkMode', isDarkMode.toString());
+    }
+  }, []);
 
   useEffect(() => {
     document.body.classList.toggle('dark', isDarkMode);
@@ -28,7 +39,7 @@ const Header = ({ index }: { index: number }) => {
 
   return (
     <div
-      className={`bg-${isDarkMode ? 'dark-' : ''}${index % 2 === 0 ? 'even' : 'odd'} w-full`}
+      className={`bg-${isDarkMode ? '' : 'dark-'}${index % 2 === 0 ? 'even' : 'odd'} w-full`}
     >
       <div className=" mx-auto max-w-7xl">
         <div className="items-center justify-between px-7 py-4 md:flex md:px-10">
@@ -40,7 +51,7 @@ const Header = ({ index }: { index: number }) => {
           {/* モバイルの時のハンバーガー */}
           <div
             onClick={() => setOpen(!open)}
-            className={`absolute right-8 top-6 h-7 w-7 cursor-pointer text-white md:hidden ${isDarkMode ? 'dark:text-black' : ''}`}
+            className={`absolute right-8 top-6 h-7 w-7 cursor-pointer  md:hidden ${isDarkMode ? 'text-white' : 'text-black'}`}
           >
             {open ? <XMarkIcon /> : <Bars3BottomRightIcon />}
           </div>
@@ -61,8 +72,8 @@ const Header = ({ index }: { index: number }) => {
                   activeClass="active"
                   smooth={true}
                   spy={true}
-                  className={`text-white transition-all duration-500 hover:text-primary ${
-                    isDarkMode ? 'dark:text-black' : ''
+                  className={`transition-all duration-500 hover:text-primary ${
+                    isDarkMode ? 'text-white ' : 'text-black'
                   }`}
                 >
                   {link.name}
@@ -70,7 +81,7 @@ const Header = ({ index }: { index: number }) => {
               </li>
             ))}
             <li
-              className={`my-7 font-semibold text-white md:my-0 md:ml-8 ${isDarkMode ? 'dark:text-black' : ''}`}
+              className={`my-7 font-semibold  md:my-0 md:ml-8 ${isDarkMode ? 'text-white' : 'text-black'}`}
             >
               <button onClick={toggleDarkMode}>
                 {isDarkMode ? 'ライトモード' : 'ダークモード'}
