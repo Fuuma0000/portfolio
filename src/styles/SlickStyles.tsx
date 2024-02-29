51ce9231dbb0c349e6d26197181176e40c3a6e41
Fuuma <masa81hiro69@gmail.com>
2024-02-29 15:45:40 +0900

    feat: react-slickのCDNのCSSを追加

---
+++ src/styles/SlickStyles.tsx
@@ -0,0 +1,17 @@
+const SlickStyles = () => (
+  <>
+    <link
+      rel="stylesheet"
+      type="text/css"
+      charSet="UTF-8"
+      href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.css"
+    />
+    <link
+      rel="stylesheet"
+      type="text/css"
+      href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick-theme.min.css"
+    />
+  </>
+);
+
+export default SlickStyles;
