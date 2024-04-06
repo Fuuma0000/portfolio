ebcf519918f4a255da1e1e6bcf3ffa324f16330e
Fuuma <masa81hiro69@gmail.com>
2024-04-06 15:04:50 +0900

    fix: dirをdist/index.htmlに変更

--- lighthouserc.cjs
+++ lighthouserc.cjs
@@ -1,7 +1,7 @@
 module.exports = {
   ci: {
     collect: {
-      staticDistDir: './dist',
+      staticDistDir: './dist/index.html',
       isSinglePageApplication: true,
       settings: { chromeFlags: '--no-sandbox', preset: 'desktop' },
     },
