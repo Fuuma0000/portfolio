e9ddfa708ed3f4392d3941067e485892c35ed4bb
Fuuma <masa81hiro69@gmail.com>
2024-04-06 16:20:16 +0900

    fix: desktopに戻した

--- lighthouserc.cjs
+++ lighthouserc.cjs
@@ -3,7 +3,7 @@ module.exports = {
     collect: {
       startServerCommand: 'bun run preview',
       url: ['http://localhost:4173/'],
-      settings: [{ chromeFlags: '--no-sandbox', preset: 'desktop' }],
+      settings: { chromeFlags: '--no-sandbox', preset: 'desktop' },
     },
     upload: {
       target: 'temporary-public-storage', // 結果をアップロードする場所の指定
