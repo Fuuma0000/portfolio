b9efe843da3e33eceaff90c44ac48b69c021a514
Fuuma <masa81hiro69@gmail.com>
2024-04-06 15:25:51 +0900

    fix: urlを追加してみた

--- lighthouserc.cjs
+++ lighthouserc.cjs
@@ -2,6 +2,7 @@ module.exports = {
   ci: {
     collect: {
       staticDistDir: './dist', // ビルドされたファイルのディレクトリ
+      url: ['http://localhost/index.html'],
       isSinglePageApplication: true, // シングルページアプリケーションの場合は true
       settings: { chromeFlags: '--no-sandbox', preset: 'desktop' }, // Lighthouseの設定
     },
