207054eca58ea71b07b425326d6e78b871bafe2f
Fuuma <masa81hiro69@gmail.com>
2024-04-06 15:11:50 +0900

    fix: dirにindex.htmlを入れないように

--- lighthouserc.cjs
+++ lighthouserc.cjs
@@ -1,12 +1,12 @@
 module.exports = {
   ci: {
     collect: {
-      staticDistDir: './dist/index.html',
-      isSinglePageApplication: true,
-      settings: { chromeFlags: '--no-sandbox', preset: 'desktop' },
+      staticDistDir: './dist', // ビルドされたファイルのディレクトリ
+      isSinglePageApplication: true, // シングルページアプリケーションの場合は true
+      settings: { chromeFlags: '--no-sandbox', preset: 'desktop' }, // Lighthouseの設定
     },
     upload: {
-      target: 'temporary-public-storage',
+      target: 'temporary-public-storage', // 結果をアップロードする場所の指定
     },
   },
 };
